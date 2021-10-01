593b03116f49aa138c7f5904b13d63efb651d909
wookim0719 <ujbae@naver.com>
2021-10-01 15:12:33 +0900

    syntax

---
+++ src/main/kotlin/syntax/Quotess.kt
@@ -0,0 +1,5 @@
+package syntax
+
+fun main() {
+    println("\"")//따옴표를 입력하기 위해서는 백슬래쉬가 필요하다
+}
---
+++ src/main/kotlin/syntax/classsimple.kt
@@ -0,0 +1,11 @@
+package syntax
+
+fun main() {
+    println("Hello");
+    println("Kotlin")
+    print("Hello\n") //println 은 줄바꿈이있고 print는 줄바꿈이 없다
+
+
+
+
+}
